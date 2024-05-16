6f467f3f04e384fe526ddd40aadad91612eebe5b
Hundredplu <130163531+Hundredplu@users.noreply.github.com>
2024-05-16 18:34:10 +0800

    Update connect.php

--- account/includes/connect.php
+++ account/includes/connect.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$servername = "awsdatabase.c7a0a8kcih2s.us-east-1.rds.amazonaws.com";
+$servername = "sqldatabase3.c168wkai0alc.us-east-1.rds.amazonaws.com";
 $server_user = "master";
 $server_pass = "master123";
 $dbname = "food";
